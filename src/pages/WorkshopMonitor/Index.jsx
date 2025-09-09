c10ae83e15a292d58928698f3253a4019523c7c7
Rahmat Riyadi <rahmatriyadi171102@gmail.com>
2025-09-09 19:23:17 +0800

    F

--- src/pages/WorkshopMonitor/Index.jsx
+++ src/pages/WorkshopMonitor/Index.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Flex, Input, message, Modal, Pagination, Select, Table, Tag, Typography } from "antd";
+import { Button, Card, Dropdown, Flex, Form, Input, message, Modal, Pagination, Popover, Select, Table, Tag, Typography, Upload } from "antd";
 import { useEffect, useState } from "react";
 import useDebounce from "../../hooks/useDebounce";
 import { keepPreviousData, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -16,10 +16,19 @@ export default function WorkshopMonitorIndex(){
 
   const [messageApi, contextHolder] = message.useMessage()
 
+  const [excelUpload, setExcelUpload] = useState([])
+  const [openModal,setOpenModal] = useState(false)
+  const [popover,setPopover] = useState(false)
   const [keyword, setKeyword] = useState("");
   const tableHeight = useTableHeight()
   const debouncedKeyword = useDebounce(keyword, 500);
   const [loadingId, setLoadingId] = useState()
+
+  const [dateFilter, setDateFilter] = useState({
+    startDate: null,
+    endDate: null,
+  })
+
   const [dataParams, setDataParams] = useState({
     limit: 10,
     page: 1,
@@ -48,10 +57,20 @@ export default function WorkshopMonitorIndex(){
   }, [debouncedKeyword]);
 
   const { data: serviceData , isLoading, isRefetching } = useQuery({
-    queryKey: ['service-data', dataParams],
-    queryFn: () => serviceDataService.fetchAll(dataParams),
+    queryKey: ['service-data', dataParams, dateFilter],
+    queryFn: () => serviceDataService.fetchAll({...dataParams, ...dateFilter}),
     placeholderData: keepPreviousData,
   })
+
+  const uploadMutation = useMutation({
+      mutationFn: () => serviceDataService.import(excelUpload[0]),
+      onSuccess: () => {
+        handleCloseModal()
+        messageApi.success("Data berhasil diimport")
+        queryClient.invalidateQueries('service-data')
+      }
+    })
+  
 
   const pagination = serviceData?.pagination || { total: 0, page: 1 };
 
@@ -82,6 +101,22 @@ export default function WorkshopMonitorIndex(){
     changeMutation.mutate({ id, MechanicId: value ? value : null })
   }
 
+  const handleCloseModal = () => {
+    setOpenModal(false)
+    setExcelUpload([])
+  }
+
+  // const handleFilterDate = () => {
+  //   queryClient.invalidateQueries('service-data')
+  //   setPopover(false)
+  // }
+
+  const handleResetFilterDate = () => {
+    setDateFilter({ startDate: null, endDate: null })
+    queryClient.invalidateQueries('service-data')
+    setPopover(false)
+  }
+
   return (
     <>
       {contextHolder}
@@ -99,6 +134,44 @@ export default function WorkshopMonitorIndex(){
             onChange={e => setKeyword(e.target.value)}
             value={keyword}
           />
+          <Popover
+            placement="bottomRight"
+            open={popover}
+            arrow={false}
+            trigger={'click'}
+            styles={{
+              body: {
+                width: 240
+              }
+            }}
+            content={
+              <>
+                  <Form.Item style={{ margin: 0, marginBottom: 10 }} label="Mulai" layout="vertical" >
+                    <Input
+                      type="date"
+                      value={dateFilter.startDate}
+                      onChange={e => setDateFilter({ ...dateFilter, startDate: e.target.value })}
+                    />
+                  </Form.Item>
+                  <Form.Item style={{ margin: 0, marginBottom: 10 }} label="Sampai" layout="vertical" >
+                    <Input
+                      type="date"
+                      value={dateFilter.endDate}
+                      onChange={e => setDateFilter({ ...dateFilter, endDate: e.target.value })}
+                    />
+                  </Form.Item>
+                  <Flex gap={10} style={{ marginTop: 20 }} >
+                    <Button onClick={handleResetFilterDate} block variant="outlined" color="danger" >
+                      Reset
+                    </Button>
+                  </Flex>
+              </>
+            }
+          >
+            <Button onClick={() => setPopover(!popover)} variant="outlined" color="primary" >
+              Filter Tanggal
+            </Button>
+          </Popover>
           <Select
             placeholder="Transfer Sales"
             options={['Ya', 'Tidak'].map(e => ({ label: e, value: e == 'Ya' }))}
@@ -113,8 +186,12 @@ export default function WorkshopMonitorIndex(){
             style={{ width: 120 }}
             onChange={val => setDataParams({ ...dataParams, mToyota: val })}
           />
+          <Button variant="solid" color="primary" onClick={() => setOpenModal(true)} >
+            Import Data
+          </Button>
         </Flex>
         <Table
+          bordered
           size="small"
           pagination={false}
           dataSource={serviceData?.data}
@@ -127,6 +204,11 @@ export default function WorkshopMonitorIndex(){
               width: 50,
               fixed: 'left',
               render: (text, record, index) =>  index + 1,
+            },
+            {
+              title: 'Tanggal Service',
+              dataIndex: 'date',
+              width: 120,
             },
             {
               title: 'No Plat & Nama',
@@ -152,7 +234,7 @@ export default function WorkshopMonitorIndex(){
               title: 'Nomor Rangka',
               dataIndex: 'chassisNumber',
               align: 'center',
-              width: 140,
+              width: 160,
             },
             {
               title: 'Transfer Sales',
@@ -164,7 +246,7 @@ export default function WorkshopMonitorIndex(){
               title: 'M Toyota',
               dataIndex: 'mToyota',
               align: 'center',
-              width: 140,
+              width: 90,
               render: val => {
                 return val ? <Tag color="green" >Sudah</Tag> : <Tag color="red" >Belum</Tag>
               }
@@ -210,7 +292,7 @@ export default function WorkshopMonitorIndex(){
             {
               className: 'last-cell-p',
               title: 'Aksi',
-              width: 110,
+              width: 90,
               fixed: 'right',
               render: (record) => (
                 <Button onClick={() => navigate(`/workshop-monitor/detail/${record.id}`)} type="primary" >
@@ -233,6 +315,39 @@ export default function WorkshopMonitorIndex(){
           onChange={handleChangePage}
         />
       </Card>
+      <Modal
+        open={openModal}
+        title="Import Data"
+        centered
+        okText="Upload"
+        onOk={uploadMutation.mutate}
+        okButtonProps={{ loading: uploadMutation.isPending }}
+        cancelButtonProps={{ loading: uploadMutation.isPending }}
+        onCancel={handleCloseModal}
+      >
+        <Upload.Dragger 
+          fileList={excelUpload}
+          style={{ paddingTop: '20px', paddingBottom: 20 }}  
+          onRemove={() => setExcelUpload([])}
+          beforeUpload={(file) => {
+            if(file.type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'){
+              messageApi.error("Harap masukan file Excel")
+              return
+            }
+            setExcelUpload([file])
+          }}
+        >
+          <Icon
+            icon="hugeicons:google-doc"
+            style={{ marginBottom: -1, marginLeft: 0 }}
+            width="32"
+            height="32"
+          />
+          <Typography.Text style={{ display: 'block' }} >
+            Tekan Atau seret file di sini untuk upload.
+          </Typography.Text>
+        </Upload.Dragger>
+      </Modal>
     </>
   )
 }
--- src/services/serviceData.service.js
+++ src/services/serviceData.service.js
@@ -2,7 +2,7 @@ import axiosInstance from "../utils/axios"
 
 export const serviceDataService = {
   fetchAll: async (params = {}) => {
-    const response = await axiosInstance.get('/service-data/today', {
+    const response = await axiosInstance.get('/service-data/paginate', {
       params,
     })
     return response.data
@@ -13,6 +13,10 @@ export const serviceDataService = {
   },
   update: async (data) => {
     const response = await axiosInstance.put(`/service-data/${data.id}`, data)
+    return response.data
+  },
+  import: async (file) => {
+    const response = await axiosInstance.postForm('/service-data/import', { file })
     return response.data
   },
 }
